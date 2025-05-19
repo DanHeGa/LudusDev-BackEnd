9feb603f06fe9b760d60778121f7455836ec5140
Collins <A01285888@tec.mx>
2025-05-18 22:50:49 -0600

    evidencias function added

--- Service/faunaTransectoService.js
+++ Service/faunaTransectoService.js
@@ -1,37 +1,52 @@
 const dataSource = require('../Datasource/MySQLMngr');
 const constants = require('../constants');
+const evidencias = require('../Service/evidenciasService');
+
 
 function getKeyByValue(object, value) {
   return Object.keys(object).find(key => object[key] === value);
 }
 
-async function insertFaunaTransecto(reqJson, idRegistro) {
-  try {
-    const query = `
-      INSERT INTO fauna_transecto 
-      (ID_registro, numeroTransecto, tipoAnimal, nombreComun, nombreCientifico, numeroIndividuos, tipoObservacion)
-      VALUES (?, ?, ?, ?, ?, ?, ?)
-    `;
+async function addFaunaPuntoConteo(reqJson) {
+    try {
+        const { unico } = await newRecord(reqJson);
 
-    const tipoAnimal = getKeyByValue(constants.tipoAnimal, reqJson.tipoAnimal);
-    const tipoObservacion = getKeyByValue(constants.tipoObservacion, reqJson.tipoObservacion);
+        const zona = getKeyByValue(constants.zona, reqJson.zona);
+        const tipoAnimal = getKeyByValue(constants.tipoAnimal, reqJson.tipoAnimal);
+        const tipoObservacion = getKeyByValue(constants.tipoObservacion, reqJson.tipoObservacion);
+        const alturaObservacion = getKeyByValue(constants.alturaObservacion, reqJson.alturaObservacion);
 
-    const params = [
-      idRegistro,
-      reqJson.numeroTransecto,
-      tipoAnimal,
-      reqJson.nombreComun,
-      reqJson.nombreCientifico,
-      reqJson.numeroIndividuos,
-      tipoObservacion
-    ];
+        const nombreComun = reqJson.nombreComun || null;
+        const nombreCientifico = reqJson.nombreCientifico || null;
+        const numeroIndividuos = parseInt(reqJson.numeroIndividuos) || 0;
 
-    const qResult = await dataSource.insertData(query, params);
-    return { qResult };
-  } catch (err) {
-    console.error(" Error en insertFaunaTransecto:", err);
-    throw err;
-  }
+        const query = `INSERT INTO fauna_punto_conteo (
+            ID_registro, zona, tipoAnimal, nombreComun, nombreCientifico,
+            numeroIndividuos, tipoObservacion, alturaObservacion
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+
+        const params = [
+            unico,
+            zona,
+            tipoAnimal,
+            nombreComun,
+            nombreCientifico,
+            numeroIndividuos,
+            tipoObservacion,
+            alturaObservacion
+        ];
+
+        const qResult = await dataSource.insertData(query, params);
+
+        // Agregar evidencia con observaciones e imágenes
+        await evidencias.newEvidenceService(unico, reqJson);
+
+        return { id_fauna_punto_conteo: qResult.getGenId(), ID_registro: unico };
+
+    } catch (error) {
+        console.error("Error en addFaunaPuntoConteo:", error);
+        throw error;
+    }
 }
 
 module.exports = { insertFaunaTransecto };
