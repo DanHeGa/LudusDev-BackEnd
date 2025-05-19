1ca2ae0ee984ad84d0fea17c1b3d4eb948171429
Angela310 <A01286354@tec.mx>
2025-05-18 19:47:43 -0600

    Angela: Agregué mis rutas

--- Controllers/router.js
+++ Controllers/router.js
@@ -15,11 +15,14 @@ const basico = require('../Controllers/API/basicRecordController')
 const constants = require("../constants");
 //API ACTIVITY CONTROLLERS 
 const camaraTramp = require('./API/camaraTrampaControllers');
+const vegetacion = require('./API/vegetacionRestControllers'); // ← Angela: Parcela Vegetación
+
 
 const router = express.Router();
 
 //API ACTIVITY ROUTES, no need of authentication in this part (for now).
 router.post(constants.contextURL + constants.apiURL + "/newCamara", camaraTramp.newCamaraTrampa);
+router.post(constants.contextURL + constants.apiURL + "/newVegetacion", vegetacion.insertVegetacion); // ← ngela: Parcela Vegetación RUTA
 router.post(constants.contextURL + constants.apiURL + "/newRecord", basico.insertRecord);
 
 router.post(constants.contextURL + constants.apiURL + "/imageUpload", imageRest.processUpload);
